0fabb38156ac90c70353e8386d619658bd2f89a7
Doyoon Lee <dynleedev@gmail.com>
2021-12-14 17:10:30 +0900

    connection between server and browser

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,18 @@
-alert("hi");
+// connection to the server
+const socket = new WebSocket(`ws://${window.location.host}`);
+
+socket.addEventListener("open", () => {
+    console.log("connected to the server");
+});
+
+socket.addEventListener("message", (message) => {
+    console.log("Just got this: ", message.data," from the server");
+});
+
+socket.addEventListener("close",()=>{
+    console.log("disconnected from the server");
+})
+
+setTimeout(()=>{
+    socket.send("hello from the broswer!");
+},3000);
--- src/server.js
+++ src/server.js
@@ -8,7 +8,7 @@ app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
 // Router
 app.use("/public", express.static(__dirname + "/public"));
-// send get rrequest to the server
+// send GET request to the server
 app.get("/", (req, res) => res.render("home"));
 app.get("/*", (req, res) => res.redirect("/"));
 
@@ -19,5 +19,21 @@ const server = http.createServer(app);
 // create ws server on top of http server
 const wss = new WebSocket.Server({ server });
 
+// socket is the browser that just connected
+wss.on("connection", (socket) => {
+    console.log("connected to browser");
+    socket.on('close', function close(code, data) {
+        const reason = data.toString();
+        console.log("disconnected from the browser");
+    });
+
+    socket.on('message', function message(data, isBinary) {
+        const message = isBinary ? data : data.toString();
+        console.log("Just got this: ",message," from the browser");
+    });
+
+    socket.send("hello!!");
+});
+
 // listen to both http and ws
-server.listen(3000,handleListen);
+server.listen(3000, handleListen);
